332a32a201a3e881b0980d3397dd23b1abbc1a1d
Foerster, T <tfoerst3r@users.noreply.github.com>
2023-05-15 10:25:51 +0200

    Removed not needed files

--- particle_conversion/cli.py
+++ particle_conversion/cli.py
@@ -23,29 +23,31 @@ def _parse_args():
     #-- create argparse object
     parser = argparse.ArgumentParser(
         description='Estimates conversion of a particle',
-        formatter_class=argparse.RawTextHelpFormatter
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     #-- add arguements to the CLI
     parser.add_argument(
-        '--config_file', 
+        '--config', 
         type=str,
+        required=True,
         help='Configuration file (.toml) - contains model and settings for running the calculation'
     )
-
-    #-- check if file exists
-    config_file = parser.parse_args().config_file
-    if not Path(config_file).is_file():
-        raise ImportError("Given config does not exists.")
-
+    
     parser.add_argument(
         '--output',
-        default='./output_' + Path(config_file).stem + '.csv', 
-        type=str,
-        help=   'Output where calculation results are stored. '
-                'Destination will be created if it does not exist, existing files will be overwritten.\n'
-                'For models "base", the output is a CSV-File\n'
+        type = str,
+        help = 'Output where calculation results are stored.',
+        default = './output_default.csv',
     ) 
+ 
+    config_file = parser.parse_args().config
+    #default = './output_' + Path(parser.parse_args().config).stem + '.csv',
+    #output_file_default = './output_' + Path(config).stem + '.csv'
+   
+    #-- check if file exists
+    if config_file and (not Path(config_file).is_file()):
+        raise ImportError("Given config does not exists.")
 
     return parser.parse_args()
 
@@ -58,12 +60,8 @@ def main():
 
     # Parse cli arguments
     args = _parse_args()
-    settings = load_config(args.config_file)
+    settings = load_config(args.config)
     output = args.output
-
-
-    # TODO: check for input errors, maybe already in load_config()
-    # check_input(settings)
 
     calculation = ZEROreactor(settings,output)
     calculation.calc()
@@ -71,15 +69,7 @@ def main():
     # important call for analysis_zeroD.py to find the EOF
     print('===EOF===')
 
-    #output = pathlib.Path(args.output)
-
-    #models_output_dir = ['multi_dim']
-    #models_output_file = ['base', 'one_dim', 'one_temp', 'one_weight']
-
-    #if model in models_output_dir and output.is_file():
-    #    sys.exit(f'Error: Output must be directory for models {models_output_dir}')
-    #if model in models_output_file and output.is_dir():
-    #    sys.exit(f'Error: Output must be a file for models {models_output_file}')
-   
     sys.exit(0)
 
+if __name__ == "__main__":
+    main()
--- particle_conversion/utils.py
+++ particle_conversion/utils.py
@@ -10,12 +10,12 @@ This file provides supporting functions:
 
 import tomli
 
-def load_config(config_file):
+def load_config(config):
     """
     Load configuration file (in toml-format) with model-settings
-    @param config_file: configuration-file with different model-settings (see documentation for details)
+    @param config: configuration-file with different model-settings (see documentation for details)
     @return: dict with settings
     """
-    with open(config_file, 'rb') as f:
-        config = tomli.load(f)
-    return config
+    with open(config, 'rb') as f:
+        config_output = tomli.load(f)
+    return config_output
